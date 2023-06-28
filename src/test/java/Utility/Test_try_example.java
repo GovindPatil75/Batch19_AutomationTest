90a08e77999dcef8dba756558fa5c9df2d7bdcfb
Roshni@123 <roshnidudhe25596@gmail.com>
2023-06-28 20:09:55 +0530

    test try example

--- src/test/java/Utility/Test_try_example.java
+++ src/test/java/Utility/Test_try_example.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class Test_try_example {
 	@Test
 	public void example() {
-		System.out.println(" try to push code");
+		System.out.println(" try to push code in repo");
 	}
 
 }
