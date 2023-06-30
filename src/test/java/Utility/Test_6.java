47f8bca0e5538e53d2119ec728ad9f43b0d68b1f
priyankalahasegit <priyankalahase@001>
2023-06-30 19:48:07 +0530

    Test Verify added

---
+++ src/test/java/Utility/Test_6.java
@@ -0,0 +1,13 @@
+package Utility;
+
+import org.testng.annotations.Test;
+
+public class Test_6 {
+
+@Test
+public void verifyloginpage() {
+	
+	System.out.println("Test");
+}
+
+}
---
+++ src/test/java/Utility/Test_git.java
@@ -0,0 +1,11 @@
+package Utility;
+
+import org.testng.annotations.Test;
+
+public class Test_git {
+@Test
+public void verifyFacebook() {
+	System.out.println("Git");
+}
+
+}
