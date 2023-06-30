43c9ae481e69b002ca8d1be78433d36fd3740f19
vaishnavibangad <vaishnavibangad27@gmail.com>
2023-06-30 20:49:02 +0530

    new test added

---
+++ src/test/java/Utility/verify_homepage.java
@@ -0,0 +1,16 @@
+package Utility;
+
+import org.testng.annotations.Test;
+
+public class verify_homepage {
+	@Test
+	public void m1() {
+		System.out.println("Test m1");
+	}
+	@Test
+	public void m2() {
+		System.out.println("Test m2");
+	}
+	}
+
+
