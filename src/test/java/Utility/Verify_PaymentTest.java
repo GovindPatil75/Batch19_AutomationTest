2ec7ba79e94d6f9734aa01303d54f7ee8c76f50f
Ashwinigithu <ashwinirajendranikam@gmail.com>
2023-06-30 09:26:48 +0530

    verifytest

--- src/test/java/Utility/Verify_PaymentTest.java
+++ src/test/java/Utility/Verify_PaymentTest.java
@@ -1,5 +1,12 @@
 package Utility;
 
+import org.testng.annotations.Test;
+
 public class Verify_PaymentTest {
 
+	
+		@Test
+		public void verifytest() {
+			System.out.println("Payment verify");
+		}
 }
