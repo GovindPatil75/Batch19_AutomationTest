77a863cb0a0b6b12d6ceeb972b27224e21454d7a
lalitdudhegithub <lalitdudhe09@gmail.com>
2023-06-30 09:44:23 +0530

    verify payment add

---
+++ src/test/java/Utility/Verify_Payment.java
@@ -0,0 +1,11 @@
+package Utility;
+
+import org.testng.annotations.Test;
+
+public class Verify_Payment {
+
+	@Test
+	public void VerifyPayment() {
+		System.out.println("Create new local branch");
+	}
+}
