c8070b7243525f9cff6b9046631e0bce31d96b90
vaishnavibangad <vaishnavibangad27@gmail.com>
2023-06-30 19:56:43 +0530

    Test_v added

---
+++ src/test/java/Utility/Test_v.java
@@ -0,0 +1,19 @@
+package Utility;
+
+import org.testng.annotations.Test;
+
+public class Test_v {
+@Test
+public void verifylogin() {
+	System.out.println("test");
+}
+public void m1() {
+	System.out.println("Test m1");
+}
+@Test
+public void m2() {
+	System.out.println("Test m2");
+}
+}
+
+
