e80195b6ac86f2a6f7db27c387140268bbf481bd
Ashwinigithu <ashwinirajendranikam@gmail.com>
2023-07-01 10:33:28 +0530

    Test 1

--- src/test/java/Utility/Test_C.java
+++ src/test/java/Utility/Test_C.java
@@ -3,14 +3,14 @@ package Utility;
 import org.testng.annotations.Test;
 
 public class Test_C {
-<<<<<<< HEAD
+
 
 	@Test
 	public void login() {
 		System.out.println("Test C");
 		
 	}
-=======
+
 	
 	@Test
 	public void verify_testA () {
@@ -18,5 +18,5 @@ public class Test_C {
 		System.out.println("Test A");
 	}
 
->>>>>>> 3ddd0223acd14a6abdd1ddaa35e2ce771502f990
+
 }
